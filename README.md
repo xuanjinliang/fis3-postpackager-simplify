21f8125c8c89e3a2a852b664513479413cb97351
禤锦良 <xuanjinliang@kugou.net>
2016-06-21 12:36:49 +0800

    添加插件

--- README.md
+++ README.md
@@ -1,1 +1,84 @@
 # fis3-postpackager-simplify
+
+### 安装
+
+```
+   npm install fis3-postpackager-simplify -g
+```
+
+### 使用
+
+```
+	//注：如果没有输入环境变量，默认为defalut
+	fis.match('::package', {
+	    postpackager:{
+	        fis.plugin('simplify',{
+	            __domain:{
+	                xjl:'xx.xxx.xx.com',
+	                test:'111.222.com',
+	                default: 'aaaa.bbbb.com'
+	            },
+	            __sign: {
+	                xjl:'123',
+	                test:'456',
+	                default: function(){
+	                    if(location.hostname.match('xx.xxx.xx.com')){
+	                        return '789';
+	                    }else{
+	                        return '1234';
+	                    }
+	                }
+	            },
+	            '__XXX|__YYY':{	//使用正则
+	                xjl:'develop',
+	                test:'test',
+	                default: 'real'
+	            }
+	        })
+	    }
+	});
+
+	//开发环境
+	fis.media('xjl').match('*.js', {
+	   ......
+	});
+	
+	/*测试环境配置*/
+	fis.media('test').match('*.js', {
+	    ......
+	});
+
+```
+
+```
+##aa.html
+	<script src="aa.js"></script><!--delete-->//注，用于写代码测试，fis3编译后，自动删除后有<!--delete-->注释的代码
+	<script type="javascript">
+        console.log(__domain());
+		console.log(__sign());
+		console.log(__XXX());
+		console.log(__YYY());
+    </script>
+```
+
+##aa.js
+```
+	function __uri(src){
+	  return src;
+	}
+	
+	var __domain = function(){
+		.....
+	}
+
+	var __sign = function(){
+		.....
+	}
+
+	var __XXX = __YYY = function(){
+		.....
+	}
+```
+### 灵感来源于fis3中的__uri
+
+(注：欢迎反馈BUG，方便提升插件的质量)
---
+++ index.js
@@ -0,0 +1,65 @@
+/**
+ * Created by timxuan on 2016/6/17.
+ */
+'use strict';
+const fs = require('fs'),
+    UglifyJS = require("uglify-js");
+
+let rStyleScript = /((<!(?:--)?\[[\s\S]*?<\!\[endif\](?:--)?>|<!--[\s\S]*?(?:-->|$))|(?:(\s*<script([^>]*)>([\s\S]*?)<\/script>)|(?:\s*(<link([^>]*?)(?:\/)?>)|(<style([^>]*)>([\s\S]*?)<\/style>))))<!--delete-->/ig;
+
+let environment = fis.project.currentMedia();   //获取当前的环境
+
+let class2type = {};
+
+"Boolean Number String Function Array Date RegExp Object Error".split(" ").forEach(function(name){
+    class2type[ "[object " + name + "]" ] = name.toLowerCase();
+});
+
+function type(val){
+    return Object.prototype.toString.call(val);
+}
+
+module.exports = (ret, conf, settings, opt) => {
+    fis.util.map(ret.src,(subpath,file) => {
+        let content = file.getContent();
+        if(file.isHtmlLike){
+            if(content.match(rStyleScript)){
+                content = content.replace(rStyleScript,'');
+                file.setContent(content);
+            }
+        }
+
+        if(file.isHtmlLike || file.isCssLike || file.isJsLike){
+            for(let i in settings){
+                if(settings.hasOwnProperty(i)) {
+                    let key = new RegExp('('+ i + ')' + '\\\(\\\)','g'),
+                        obj = settings[i],
+                        parameter = obj[environment] || obj.default;
+
+                    if(parameter){
+                        if(content.match(key)){
+                            let replaceStr = '';
+                            switch(class2type[type(obj[environment])]){
+                                case 'function':
+                                    let format = UglifyJS.minify("var a = "+obj[environment], {fromString: true}).code.match(/function\(\){.+}/);
+                                    replaceStr = '(' + format + ')()';
+                                    break;
+                                case 'string':
+                                    replaceStr = '\''+obj[environment]+'\'';
+                                    break;
+                                default:
+                                    replaceStr = obj[environment];
+                                    break;
+                            }
+                            if(replaceStr){
+                                content = content.replace(key,replaceStr);
+                                file.setContent(content);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    });
+};
+
